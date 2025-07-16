21a366843ab8f99cab5265d47898fc0a538bcddd
arpitKakran <arpitinsan77@gmail.com>
2025-07-16 18:27:20 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from'./pages/Movies'
+import MovieDetails from "./pages/MovieDetails"
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
+import {Toaster} from 'react-hot-toast'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
+
+function App() {
+
+  const isAdminRoute= useLocation().pathname.startsWith('/admin') //it will give only true and false value
+
+  return (
+    <>
+       <Toaster/> {/*to use notification on any components */}
+      {!isAdminRoute && <Navbar/>} {/*if admin path then hide navbar else show it*/} 
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>{/* here we have passed /:id just to use it while accessing the id from params to make sure that id gets displayed here we will add on click function on the movie card image section */}
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favourite' element={<Favourite/>}/>
+        
+        <Route path='/admin/*' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='add-shows' element={<AddShows/>}/>
+          <Route path='list-shows' element={<ListShows/>}/>
+          <Route path='list-bookings' element={<ListBookings/>}/>
+        </Route>
+      
+      </Routes>
+      {!isAdminRoute && <Footer/>}
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/featureSection'
+import TrailerSection from '../components/TrailerSection'
+
+
+
+const Home= ()=> {
+  return (
+    <>
+      <HeroSection/>
+      <FeatureSection/>
+      <TrailerSection/>
+    </>
+  )
+}
+
+export default Home
