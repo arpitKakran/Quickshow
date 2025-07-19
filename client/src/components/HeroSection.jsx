3677f3bcf55a672638ddb66b3a1b7c638a624fd8
arpitKakran <arpitinsan77@gmail.com>
2025-07-19 22:04:36 +0530

    Implement code changes to enhance functionality and improve performance

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     const navigate= useNavigate()
 
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("background.png")] bg-cover bg-center h-screen'>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/background.png")] bg-cover bg-center h-screen'>
         <img src={assets.marvelLogo} alt='' className='max-h-11 lg:h-11 mt-20'/>
         <h1 className='text-4xl md:text-[65px] md:leading-18 font-semibold max-w-150'> Captain America</h1>
         <h2 className='text-3xl md:text-[50px] md:leading-18 font-semibold max-w-150'> Brave New World</h2>
